48d42990766a02dac7bc3dbda8be11f2228ccc6d
Maxim Korotkikh <m.korotkikh@ongrid.pro>
2021-06-15 23:44:38 +0300

    Refactor components to Class

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,30 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 
-const TodoListItem = ( { label, important=false } ) => {
-    const itemStyle = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal',
-    }
 
-    return (
-        <span className='todo-list-item'>
-            <span className="todo-list-item-label"
-                style={itemStyle}>
-                { label }
+export default class TodoListItem extends Component {
+
+    render() {
+        const { label, important } = this.props;
+        const itemStyle = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal',
+        };
+
+        return (
+            <span className='todo-list-item'>
+                <span className='todo-list-item-label'
+                      style={ itemStyle }>
+                    { label }
+                </span>
+                <button type='button'
+                        className='btn btn-outline-danger btn-sm float-right'>
+                    <i className="fa fa-trash-o"></i>
+                </button>
+                <button type='button'
+                        className='btn btn-outline-success btn-sm float-right'>
+                    <i className='fa fa-exclamation'></i>
+                </button>
             </span>
-            
-            <button type='button'
-                    className='btn btn-outline-danger btn-sm float-right'>
-                <i className="fa fa-trash-o"></i>
-            </button>
-            <button type='button'
-                    className='btn btn-outline-success btn-sm float-right'>
-                <i className='fa fa-exclamation'></i>
-            </button>
-            
-        </span>
-    )
+        )
+    }
 };
-
-export default TodoListItem;
